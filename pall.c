a605921f0eb35b5a6bb7177e379018c51c8d461f
happifeetty#@gmail.com <tokayode@gmail.com>
2022-12-15 13:24:29 -0800

    update

--- pall.c
+++ pall.c
@@ -1,16 +1,42 @@
 #include "monty.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
- * op_pall - print the value in the nodo
- * @stack: struct of the doubly linked list
- * @line_number: number of the line in the file.
+ * pall - prints all the values on the stack, starting
+ * from the top of the stack
+ *
+ * @stack: stack
+ * @line_number: line number to use while printing error
+ *
+ * Return: void
  */
-void op_pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack, unsigned int line_number)
 {
-stack_t *tmp = *stack;
-(void) line_number;
-while (tmp != NULL)
+stack_t *node;
+(void)line_number; /* unused parameter */
+node = getTail(stack); /* get the top node */
+while (node != NULL)
 {
-printf("%d\n", tmp->n);
-tmp = tmp->next;
+printf("%d\n", node->n); /* print element */
+node = node->prev; /* go to previous node */
 }
+}
+/**
+ * pint - print the number in the top of the stack
+ * @stack: stack
+ * @line_number: line number to use for printing error
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+stack_t *tail;
+if (stack == NULL || *stack == NULL)
+{
+dprintf(STDERR_FILENO, "L%u: can't pint, stack empty\n", line_number);
+stackFree(stack);
+exit(EXIT_FAILURE);
+}
+tail = getTail(stack);
+printf("%i\n", tail->n);
 }
